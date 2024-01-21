d255934eb6d56a7c4e810129e970947aa2182ed2
TrishanRaj <121537819+TrishanRaj@users.noreply.github.com>
2024-01-21 19:43:55 +0800

    Update BookingGui.java

--- BookingGui.java
+++ BookingGui.java
@@ -2,6 +2,8 @@ package flightbook;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.temporal.WeekFields;
+import java.util.Locale;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -28,21 +30,26 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.util.Optional;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DateCell;
+import javafx.scene.control.Toggle;
 import javafx.util.Callback;
 
 
 public class BookingGui extends Stage {  
 
     private Label lblTitle, lblName, lblPassNo, lblPhoneNum, lblFlightName, lblFlightTime, lblGender,lblDestination;
-    private TextField txtName, txtPassNo, txtPhoneNum, txtFlightName,txtDestination;
+    private TextField txtName, txtPassNo, txtPhoneNum, txtFlightName,txtDestination,txtflightTime,txtflightDate ;
     private ToggleGroup genderGroup;
     private RadioButton rbMale, rbFemale;
     private DatePicker datePicker;
+    private ComboBox<String> flightComboBox;
 
-    public BookingGui(String username,Ticket[] book, MyQueue<Ticket> waitList) {
+    public BookingGui(String username,Ticket[] book, MyQueue<Ticket> waitList,Flight[] flight) {
 
         setTitle("Malaya Flight Booking System"); 
         Button btnBack = new Button("Go Back");
@@ -83,7 +90,8 @@ public class BookingGui extends Stage {
         rbFemale = new RadioButton("Female");
         rbFemale.setToggleGroup(genderGroup);
         infoVBox.getChildren().add(new HBox(120, lblGender, new HBox(10, rbMale, rbFemale)));
-
+        
+      
         lblPhoneNum = new Label("Phone Number:");
         txtPhoneNum = new TextField();
         txtPhoneNum.setPrefWidth(250); 
@@ -100,15 +108,21 @@ public class BookingGui extends Stage {
         
        
 
-        lblFlightTime = new Label("Flight Date: ");
+        lblFlightTime = new Label("Expected Date: ");
         datePicker = new DatePicker();
         datePicker.setPrefWidth(250); 
-        infoVBox.getChildren().add(new HBox(75, lblFlightTime, datePicker));
+        infoVBox.getChildren().add(new HBox(60, lblFlightTime, datePicker));
         
        //Set the time only to January
         EventHandler<ActionEvent> eventHandler = event -> {
             LocalDate selectedDate = datePicker.getValue();
             if (selectedDate != null) {
+                 WeekFields weekFields = WeekFields.of(Locale.getDefault());
+                int weekNumber = selectedDate.get(weekFields.weekOfWeekBasedYear());
+
+                // Update the flight options based on the selected week
+                updateFlightOptions(weekNumber, flight);
+                
                 if (selectedDate.getYear() != 2024 || selectedDate.getMonth() != Month.JANUARY) {
                     datePicker.setValue(LocalDate.of(2024, Month.JANUARY, 1));
                 }
@@ -132,10 +146,27 @@ public class BookingGui extends Stage {
 
         datePicker.setDayCellFactory(dayCellFactory);
         
+                
+        
          lblFlightName = new Label("Flight Name: ");
-        txtFlightName = new TextField();
-        txtFlightName.setPrefWidth(250); 
-        infoVBox.getChildren().add(new HBox(68, lblFlightName, txtFlightName));
+         flightComboBox = new ComboBox<>();
+        flightComboBox.setPrefWidth(250);
+        infoVBox.getChildren().add(new HBox(68, lblFlightName, flightComboBox));
+
+        Label lblFlightDate = new Label("Flight Date: ");
+        txtflightDate= new TextField(); // You may replace this with a proper time picker
+        txtflightDate.setPrefWidth(250);
+        txtflightDate.setEditable(false);
+        infoVBox.getChildren().add(new HBox(70, lblFlightDate, txtflightDate));
+
+        Label lblFTime = new Label("Flight Time: ");
+        txtflightTime = new TextField(); // You may replace this with a proper time picker
+        txtflightTime.setPrefWidth(250);
+        txtflightTime.setEditable(false);
+        infoVBox.getChildren().add(new HBox(70, lblFTime, txtflightTime));
+
+
+
 
         // GridPane for labels and text fields
         GridPane infoGrid = new GridPane();
@@ -163,30 +194,73 @@ public class BookingGui extends Stage {
         VBox rootVBox = new VBox(20, mainVBox, infoGrid, buttonBox);
         rootVBox.setAlignment(Pos.TOP_CENTER);
             
-        setScene(new Scene(rootVBox, 850, 550));
+        setScene(new Scene(rootVBox, 1050, 600));
          setResizable(false);  
+         
         
         btnBack.setOnAction(e -> {
-            openMenuGui(username,book,waitList);
+            openMenuGui(username,book,waitList,flight);
         });
         
 
+                        flightComboBox.setOnAction(event -> {
+             String selectedFlightInfo = flightComboBox.getValue();
+
+             if (selectedFlightInfo != null) {
+                 // Extract flight name from selectedFlightInfo
+                 String selectedFlightName = selectedFlightInfo.split(" -- ")[0];
+
+                 // Find the corresponding flight and update the text fields
+                 for (Flight test : flight) {
+                     if (flight != null && test.flightName.equals(selectedFlightName)) {
+                         txtflightTime.setText(test.flightTime);
+                         txtflightDate.setText(test.flightDate);
+                         break;
+                     }
+                 }
+             }
+         });
+                
         // Event handler for the Book button
         btnBook.setOnAction(e -> {
             // Check if all required fields are filled
             if (isInputValid()) {
+                
+                Toggle selectedToggle = genderGroup.getSelectedToggle();
+                RadioButton selectedRadioButton = (RadioButton) selectedToggle;
+                
                 // Get passenger details
                 String passengerName = txtName.getText();
                 String passportNo = txtPassNo.getText();
                 String phoneNum = txtPhoneNum.getText();
-                String flightName = txtFlightName.getText();
-                String flightTime = datePicker.getValue().toString();
+                String gender = selectedRadioButton.getText();
+                String flightInfo = flightComboBox.getValue();
+              String[] flightInfoParts = flightInfo.split(" -- ");
+              String flightName = flightInfoParts[0];
+              String flightTime = txtflightTime.getText();
+              String flightDate=txtflightDate.getText();
+              String departure="Malaysia";
+              String arrival="South Korea";
+              
+          
+            Flight selectedFlight = null;
+        for (Flight test : flight) {
+            if (test != null && test.flightName.equals(flightName)) {
+                selectedFlight = test;
+                break;
+            }
+        }
 
-                // Create Ticket object and book the ticket
-                Ticket myTicket = new Ticket(passengerName, passportNo, phoneNum, username, flightName, flightTime, null);
+        if (selectedFlight != null) {
+            // Decrement the flight capacity
+            selectedFlight.flightCapacity--;
 
+            // Create Ticket object and book the ticket
+            Ticket myTicket = new Ticket(passengerName, passportNo, phoneNum, username, flightName, flightDate, flightTime, null, gender, departure, arrival);
+
+            boolean booked = false;
+            if (selectedFlight.flightCapacity >= 0) {
                 // Find the first null entry in the book array and book the ticket
-                boolean booked = false;
                 for (int a = 0; a < book.length; a++) {
                     if (book[a] == null) {
                         book[a] = myTicket;
@@ -194,25 +268,23 @@ public class BookingGui extends Stage {
                         break;
                     }
                 }
-
-                if (booked) {
-                    
-                    myTicket.status = "Confirmed";
-                    showSuccessMessage();
-                    openMenuGui(username,book,waitList);
-                                        
-                } 
-                else {
-                   
-                    myTicket.status = "Waiting";
-                    waitList.enqueue(myTicket);
-                    showSuccessMessage(); // Display success 
-                    openMenuGui(username,book,waitList);
-                   
-                }
-              
             }
-        });
+
+            if (booked) {
+                myTicket.status = "Confirmed";
+                showSuccessMessage();
+                openMenuGui(username, book, waitList, flight);
+            } else {
+                myTicket.status = "Waiting";
+                waitList.enqueue(myTicket);
+                showSuccessMessage();
+                openMenuGui(username, book, waitList, flight);
+            }
+        } else {
+            showAlert(Alert.AlertType.ERROR, "Error Message", "Selected flight not found!");
+        }
+    }
+});
 
         // Event handler for the Cancel button
         btnCancel.setOnAction(e -> {
@@ -220,7 +292,7 @@ public class BookingGui extends Stage {
             Optional<ButtonType> result = showConfirmationAlert("Cancel Booking", "Are you sure you want to cancel?");
             if (result.isPresent() && result.get() == ButtonType.OK) {
                 // User confirmed canceling, close the stage
-                openMenuGui(username,book,waitList);
+                openMenuGui(username,book,waitList,flight);
             }
         });
     }
@@ -249,8 +321,10 @@ public class BookingGui extends Stage {
 
     private boolean isInputValid() {
         // Perform validation checks here
+          Toggle selectedToggle = genderGroup.getSelectedToggle();
         if (txtName.getText().isEmpty() || txtPassNo.getText().isEmpty() || txtPhoneNum.getText().isEmpty()
-                || txtFlightName.getText().isEmpty() || datePicker.getValue() == null) {
+                || flightComboBox.getValue() == null|| selectedToggle==null||datePicker.getValue() == null|| txtflightTime.getText().isEmpty()
+                ||txtflightDate.getText().isEmpty()) {
             showAlert(AlertType.ERROR, "Error Message", "Please fill in all the required fields.");
             return false;
         }
@@ -265,9 +339,21 @@ public class BookingGui extends Stage {
         return alert.showAndWait();
     }
     
+     private void updateFlightOptions(int selectedWeek, Flight[] flights) {
+        ObservableList<String> availableFlights = FXCollections.observableArrayList();
+
+        for (Flight flight : flights) {
+            if (flight != null && Integer.parseInt(flight.flightWeek) == selectedWeek) {
+                String flightdetails = flight.flightName + " -- Left Seats: " + flight.flightCapacity;
+                availableFlights.add(flightdetails);
+            }
+        }
+
+        flightComboBox.setItems(availableFlights);
+    }
  
-    private void openMenuGui(String userName,Ticket[] book, MyQueue<Ticket> waitList) {
-        MenuGui menuGui = new MenuGui(userName,book,waitList);
+    private void openMenuGui(String userName,Ticket[] book, MyQueue<Ticket> waitList,Flight[] flight) {
+        MenuGui menuGui = new MenuGui(userName,book,waitList,flight);
         menuGui.show();
         close(); // Close the current LoginGui window
     }
